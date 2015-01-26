d9125d0053b5a36ad5599e867608cc7ed30a1a68
Chad H <chadroberthall@gmail.com>
2015-01-25 19:49:54 -0700

    1st Commit

--- js/countdown.js
+++ js/countdown.js
@@ -1,11 +1,40 @@
-//change the text below to reflect your own,
+
+//chadly was here
 var before = "Groundhog's Day!";
 var current = "Today is Groundhog's Day!";
 var montharray = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 
-function countdown(yr, m, d) {
- // ...
+
+function countdown(yr,m,d){
+    document.body.innerHTML = '';
+
+var today=new Date()
+var tdy=today.getYear()
+var tdh = today.getHours();
+var tdmin = today.getMinutes();
+var tdsec = today.getSeconds();
+if (tdy < 1000)
+tdy+=1900
+var tdmon=today.getMonth()
+var tdd=today.getDate()
+var todaystring=montharray[tdmon]+" "+tdd+", "+tdy+ " " + tdh + ":" + tdmin + ":" + tdsec;
+var futurestring=montharray[m-1]+" "+d+", "+yr+", "+0 + ":" + 0 + ":" + 0;
+var diffd=(Math.round((Date.parse(futurestring)-Date.parse(todaystring))/(24*60*60*1000))*1)
+var diffh=(Math.round(((Date.parse(futurestring)-Date.parse(todaystring))% (60 * 60 * 1000 * 24)) / (60 * 60 * 1000) * 1))
+var diffm=(Math.round((((Date.parse(futurestring)-Date.parse(todaystring))% (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) / (60 * 1000) * 1))
+var diffs=(Math.round(((((Date.parse(futurestring)-Date.parse(todaystring))% (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) % (60 * 1000)) / 1000 * 1))
+
+if (diffd<0&&(diffh-1)<0&&(diffm-1)<0&&diffs<1)
+{
+document.write(current)
+clearInterval(t);
+}
+else if (diffs>0)
+document.write("Only "+diffd+" days, "+(diffh-1)+" hours, "+(diffm-1)+" min, and "+diffs+" seconds till "+before)
+
+
 }
 
 //enter the count down date using the format year/month/day
-countdown(2015, 2, 2);
+var myVar=setInterval(function () {countdown(2015, 2, 2)}, 1000);
+//countdown(2015, 2, 2);
--- php/countdown.php
+++ php/countdown.php
@@ -1,14 +1,20 @@
 <?php
-
+//chadly was here. 
 if (PHP_SAPI !== 'cli') {
     die (PHP_EOL.'Please run '.__FILE__.' from the command-line.'.PHP_EOL.PHP_EOL);
 }
 
-function countdown($seconds)
-{
-    // ...
+function countdown($seconds) {
+    for ($i=$seconds; $i>=0; $i--) {
+        echo "\r"; 
+        echo "$i "; //show number
+        
+        sleep(1);
+    }
+    echo "\r  "; //clear and overwrite it
 }
 
 countdown(5);
 
 die('0 All done...');
+?>
